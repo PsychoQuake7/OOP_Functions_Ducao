5b246fe211f35e30f121742c0de440221d1d91bb
Calaisha Mae S. Ducao <109143325+PsychoQuake7@users.noreply.github.com>
2024-09-20 06:26:13 -0800

    Update Program.cs

--- Function_1/Program.cs
+++ Function_1/Program.cs
@@ -1,1 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Practice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+                heron hero = new heron();
+
+                // Example sides 
+                double a = 5;
+                double b = 10;
+                double c = 7;
+
+                double area = hero.HeronTriangleArea(a, b, c);
+
+                Console.WriteLine("The area of the triangle is: " + area);
+
+                Console.ReadKey();
+            
+        }
+
+        public class heron
+        {
+            public double HeronTriangleArea(double a, double b, double c)
+            {
+                double s = (a + b + c) / 2;
+
+                double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+                return area;
+            }
+
+        }
+    }
+
+}
